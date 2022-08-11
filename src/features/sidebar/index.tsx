a2c395322b9c01c79097c21d89fac7ba0ea048cb
guempilKo <ko11380@visionerp.org>
2022-08-11 18:42:22 +0900

    push

--- src/features/sidebar/index.tsx
+++ src/features/sidebar/index.tsx
@@ -1,3 +1,19 @@
+import { styled, useMediaQuery } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { display, minWidth } from "@mui/system";
+import Item from "components/item";
+import { NONAME } from "dns";
+
 export const SideBar = () => {
-  return <></>;
+  return (
+    <SideBarBase xs={1.5}>
+      <Item>Side</Item>
+    </SideBarBase>
+  );
 };
+
+const SideBarBase = styled(Grid2)(() => ({
+  minWidth: 250,
+
+  useMediaQuery,
+}));
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,28 +1,79 @@
-import { Box, styled, Paper } from "@mui/material";
+import {
+  Box,
+  Button,
+  createTheme,
+  PaletteMode,
+  ThemeProvider,
+} from "@mui/material";
+import { amber, deepOrange, grey } from "@mui/material/colors";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import Item from "components/item";
+import { SideBar } from "features/sidebar";
+import React from "react";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === "dark" && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === "dark" && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === "light"
+        ? {
+            primary: grey[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: "#fff",
+            secondary: grey[500],
+          }),
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
+  const [mode, setMode] = React.useState<PaletteMode>("light");
+  const colorMode = React.useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      },
+    }),
+    []
+  );
+  // Update the theme only if the mode changes
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
         <Grid2 container spacing={1}>
-          <Grid2 xs={3}>
-            <Item>Side</Item>
-          </Grid2>
+          <SideBar />
           <Grid2 xs>
-            <Item>Main</Item>
+            <Item>
+              <Button onClick={colorMode.toggleColorMode}>switch</Button>
+            </Item>
           </Grid2>
         </Grid2>
       </Box>
-    </>
+    </ThemeProvider>
   );
 };
 
